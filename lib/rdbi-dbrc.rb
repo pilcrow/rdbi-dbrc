00927eef67f40f71fe34431304bd1b9eb571b7d3
pilcrow <mjp@pilcrow.madison.wi.us>
2011-01-26 01:12:53 -0600

    Document new loading behavior, dbrc_load config key.

--- lib/rdbi-dbrc.rb
+++ lib/rdbi-dbrc.rb
@@ -8,23 +8,44 @@ module RDBI # :nodoc:
   # If you know a little YAML, using DBRC is easy:
   #
   # Connections are keyed by a role and then subkeyed by connection parameters,
-  # with a few exceptions which are not passed along: 
+  # with a few exceptions which are not passed along:
   #
-  # driver:: corresponds to the driver you wish to use. This driver must
-  #          already be loaded before the role is leveraged.
+  # driver::    corresponds to the driver you wish to use.  This driver will be
+  #             downcased and loaded unless +dbrc_load+ is specified.
+  # dbrc_load:: ruby library to load instead of the implicit driver library.
+  #             If omitted, the default driver library name is loaded.  If
+  #             specified but empty (nil), no driver is loaded.  Otherwise,
+  #             loads the named library (or libraries, if a YAML list).
+  # dbrc_*::    keys beginning with +dbrc_+ are reserved for future RDBI::DBRC
+  #             use, and are currently ignored.
   #
-  # For example:
+  # For example, given:
   #
+  #     ---
   #     memory_connection:
-  #       driver: SQLite3
+  #       driver: SQLite3      # loads 'rdbi/driver/sqlite3'
   #       database: ":memory:"
+  #     experimental:
+  #       driver: FauxSQL      # does not load '.../fauxsql' nor any driver
+  #       dbrc_load:
+  #       ...
+  #     locally_patched:
+  #       driver: Unreliable
+  #       dbrc_load:
+  #         - rdbi/driver/unreliable
+  #         - local/patches/workaround
+  #       ...
   #
-  # RDBI::DBRC.connect will call RDBI.connect like so:
+  # RDBI::DBRC.connect(:memory_connection) would call RDBI.connect like so:
   #
   #     RDBI.connect(:SQLite3, :database => ":memory:")
   #
-  # The file lives by default in $HOME/.dbrc. Don't like that? Set the +DBRC+
-  # environment parameter:
+  # .connect(:experimental) would load no drivers whatsoever, whereas
+  # .connect(:locally_patched) would load 'rdbi/driver/unreliable' followed
+  # by 'local/patches/workaround' before issuing the underlying connect().
+  #
+  # The configuration file lives by default in $HOME/.dbrc. Don't like that?
+  # Set the +DBRC+ environment variable:
   #
   #     DBRC=/tmp/dbrc my_rdbi_program.rb
   #
