152c4768dc62775b3ceb13a4db71825a07a22f5c
pilcrow <mjp@pilcrow.madison.wi.us>
2011-01-26 00:42:21 -0600

    Add module_loaded?() method, add 'fauxlib' to $:

--- test/helper.rb
+++ test/helper.rb
@@ -1,15 +1,18 @@
 require 'rubygems'
-gem 'test-unit'
 require 'test/unit'
-gem 'rdbi-driver-mock'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'fauxlib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rdbi-dbrc'
-require 'rdbi/driver/mock'
 
 class Test::Unit::TestCase
   def setup
     ENV["DBRC"] = File.join(File.dirname(__FILE__), 'dbrc')
+  end
+
+  private
+  def module_loaded?(name)
+    eval "defined?(#{name}) && #{name}.is_a?(::Module)"
   end
 end
